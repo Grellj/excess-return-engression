370d7defab2ed2900a17da95b499219545b98943
Johannes Grell <Grellj@students.uni-marburg.de>
2026-03-25 21:13:54 +0100

    Modified ere_runner to include different batch sizes
    Edited ere_runner, adjusted ere_input_visualizer, results folder now contains run outputs using batch sizes 32, 64, 128, 256 and 512.

--- ere_input_visualizer.py
+++ ere_input_visualizer.py
@@ -6,3 +6,4 @@ with open("results/input_info.txt", "w") as input_info:
     input_info.write(str(ere_dataprep.X_tensor.shape))
     input_info.write("\nThe shape of the Y-tensor used in training is: \n")
     input_info.write(str(ere_dataprep.Y_tensor.shape))
+    input_info.write("\nNote that different batch sizes are currently being implemented in ere_runner.py and thus not displayed here. \n")
--- ere_runner.py
+++ ere_runner.py
@@ -12,10 +12,13 @@ class ERERunnerVisualizer:
         self.output1.write(output)
         self.output2.write(output)
 
+batch_sizes = [32, 64, 128, 256, 512]
 console_output = sys.stdout
-file_output= open("results/model_run_overview.txt", "w")
-ere_runner_visualizer = ERERunnerVisualizer(console_output, file_output)
-sys.stdout = ere_runner_visualizer
-#  Constructs and runs the training experiment itself.
-ere_model = engression(ere_dataprep.X_tensor, ere_dataprep.Y_tensor, num_epochs=1000, print_every_nepoch=50)
-file_output.close()
+for bs in batch_sizes:
+    file_output= open("results/model_run_overview_bs" + str(bs) + ".txt", "w")
+    ere_runner_visualizer = ERERunnerVisualizer(console_output, file_output)
+    sys.stdout = ere_runner_visualizer
+    #  Constructs and runs the training experiment itself.
+    ere_model = engression(ere_dataprep.X_tensor, ere_dataprep.Y_tensor, num_epochs=1000, batch_size=bs, print_every_nepoch=50)
+    sys.stdout = console_output
+    file_output.close()
