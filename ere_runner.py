67869b925647cd0542a70cfa8ecb03d801bc9291
Johannes Grell <Grellj@students.uni-marburg.de>
2026-04-14 00:13:02 +0200

    Adds different learning rates and evaluation steps mid training
    Modified ere_runner.py to run model iterations with learning rates 0.001, 0.0001 and 0.00001. Split training into 10 blocks of 100 epochs each, evaluating the model on the validation set after each block. Generated model outputs for all 15 hyperparameter combinations.

--- ere_runner.py
+++ ere_runner.py
@@ -12,18 +12,25 @@ class ERERunnerVisualizer:
         self.output1.write(output)
         self.output2.write(output)
 
+learning_rates = [0.001, 0.0001, 0.00001]
 batch_sizes = [32, 64, 128, 256, 512]
 console_output = sys.stdout
 for bs in batch_sizes:
-    file_output= open("results/model_run_overview_bs" + str(bs) + ".txt", "w")
-    ere_runner_visualizer = ERERunnerVisualizer(console_output, file_output)
-    sys.stdout = ere_runner_visualizer
-    #  Constructs and runs the training experiment itself.
-    ere_model = engression(ere_dataprep.X_train, ere_dataprep.Y_train, num_epochs=1000, batch_size=bs, print_every_nepoch=50)
-    # Evaluates model performance on the validation set
-    print("Energy loss evaluation")
-    print(ere_model.eval_loss(ere_dataprep.X_validate, ere_dataprep.Y_validate, loss_type="energy", verbose=True))
-    print("L2 loss evaluation")
-    print(ere_model.eval_loss(ere_dataprep.X_validate, ere_dataprep.Y_validate))
-    sys.stdout = console_output
-    file_output.close()
+    for l in learning_rates:
+        file_output= open("results/model_run_overview_bs" + str(bs) + "lr" + str(l) + ".txt", "w")
+        ere_runner_visualizer = ERERunnerVisualizer(console_output, file_output)
+        sys.stdout = ere_runner_visualizer
+        #  Constructs and runs the first 100-epoch block of the training experiment itself.
+        ere_model = engression(ere_dataprep.X_train, ere_dataprep.Y_train, lr = l, num_epochs=50, batch_size=bs)
+        # Evaluates model performance after first training using the validation set
+        print("Energy loss evaluation")
+        print(ere_model.eval_loss(ere_dataprep.X_validate, ere_dataprep.Y_validate, loss_type="energy", verbose=True))
+        # subsequent 9 blocks of training and evaluation
+        for i in range(1, 10):
+            ere_model.train(ere_dataprep.X_train, ere_dataprep.Y_train, num_epochs=50, batch_size=bs)
+            print("Energy loss evaluation")
+            print(ere_model.eval_loss(ere_dataprep.X_validate, ere_dataprep.Y_validate, loss_type="energy", verbose=True))
+        print("L2 loss evaluation")
+        print(ere_model.eval_loss(ere_dataprep.X_validate, ere_dataprep.Y_validate))
+        sys.stdout = console_output
+        file_output.close()
