f7c1700adde09be8c95ba973e1a0c9cb4940dcc3
Johannes Grell <Grellj@students.uni-marburg.de>
2026-05-09 02:33:57 +0200

    Changes to runner and results folder
    Renamed ere_runner.py to main.py, slightly adjusted lr-float declaration to reflect scientific practice and set total number of epochs to 100. Created subfolders input_info and output_info in results, the latter containing a best block performance overview which is created during model evaluation. Changed ere_input_visualizer.py and main.py accordingly.

--- ere_input_visualizer.py
+++ ere_input_visualizer.py
@@ -1,17 +1,17 @@
 import ere_dataprep
 
 # Serves to display essential information about the training data in the results folder.
-with open("results/input_info.txt", "w") as input_info:
-    input_info.write("the shape of the X-tensor used in training is: \n")
-    input_info.write(str(ere_dataprep.X_train.shape))
-    input_info.write("\nThe shape of the Y-tensor used in training is: \n")
-    input_info.write(str(ere_dataprep.Y_train.shape))
-    input_info.write("\nThe shape of the X-tensor used for validation is: \n")
-    input_info.write(str(ere_dataprep.X_validate.shape))
-    input_info.write("\nThe shape of the Y-tensor used for validation is: \n")
-    input_info.write(str(ere_dataprep.Y_validate.shape))
-    input_info.write("\nThe shape of the X-tensor used for final testing is: \n")
-    input_info.write(str(ere_dataprep.X_test.shape))
-    input_info.write("\nThe shape of the Y-tensor used for final testing is: \n")
-    input_info.write(str(ere_dataprep.Y_test.shape))
-    input_info.write("\nNote that batch sizes 32, 64, 128, 256 and 512 are being implemented in ere_runner.py and thus not displayed here. \n")
+with open("results/input_info/input_shapes.txt", "w") as input_shapes:
+    input_shapes.write("the shape of the X-tensor used in training is: \n")
+    input_shapes.write(str(ere_dataprep.X_train.shape))
+    input_shapes.write("\nThe shape of the Y-tensor used in training is: \n")
+    input_shapes.write(str(ere_dataprep.Y_train.shape))
+    input_shapes.write("\nThe shape of the X-tensor used for validation is: \n")
+    input_shapes.write(str(ere_dataprep.X_validate.shape))
+    input_shapes.write("\nThe shape of the Y-tensor used for validation is: \n")
+    input_shapes.write(str(ere_dataprep.Y_validate.shape))
+    input_shapes.write("\nThe shape of the X-tensor used for final testing is: \n")
+    input_shapes.write(str(ere_dataprep.X_test.shape))
+    input_shapes.write("\nThe shape of the Y-tensor used for final testing is: \n")
+    input_shapes.write(str(ere_dataprep.Y_test.shape))
+    input_shapes.write("\nNote that different batch sizes are being implemented in main.py and thus not displayed here. \n")
--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+import sys
+import ere_dataprep
+from third_party.engression.engression import engression
+import csv
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# This script serves to run model experiments and save their output
 
+# Creates csv file to save best training block result for each of the model runs
+with open("results/output_info/output_info.csv", "w", newline= "") as output_info:
+    output_writer = csv.writer(output_info)
+    output_writer.writerow(["Batch Size", "Learning Rate", "Validation Energy Loss", "Achieved in Block"])
+# Ensures that the model output is saved in the results folder as well as printed on the console during training
+class ERERunnerVisualizer:
+    def __init__(self, output1, output2):
+        self.output1 = output1
+        self.output2 = output2
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    def write(self, output):
+        self.output1.write(output)
+        self.output2.write(output)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+batch_sizes = [32, 64, 128, 256, 512]
+learning_rates = [1e-03, 1e-04, 1e-05, 1e-06, 1e-07]
+console_output = sys.stdout
+for bs in batch_sizes:
+    for l in learning_rates:
+        file_output= open("results/output_info/model_run_overview_bs" + str(bs) + "lr" + str(l) + ".txt", "w")
+        ere_runner_visualizer = ERERunnerVisualizer(console_output, file_output)
+        sys.stdout = ere_runner_visualizer
+        #  Constructs and runs the first 10-epoch block of the training experiment itself.
+        ere_model = engression(ere_dataprep.X_train, ere_dataprep.Y_train, lr = l, num_epochs=10, batch_size=bs)
+        # Evaluates model performance after first training using the validation set
+        print("Energy loss evaluation")
+        current_e_values = ere_model.eval_loss(ere_dataprep.X_validate, ere_dataprep.Y_validate, loss_type="energy", verbose = True)
+        print(current_e_values)
+        current_block = 1
+        # subsequent 9 blocks of training and evaluation
+        for b in range(2, 11):
+            ere_model.train(ere_dataprep.X_train, ere_dataprep.Y_train, num_epochs=10, batch_size=bs)
+            print("Energy loss evaluation")
+            new_e_values = ere_model.eval_loss(ere_dataprep.X_validate, ere_dataprep.Y_validate, loss_type="energy", verbose = True)
+            print(new_e_values)
+            if new_e_values[0] < current_e_values[0]:
+                current_e_values = new_e_values
+                current_block = b
+        print("L2 loss evaluation")
+        print(ere_model.eval_loss(ere_dataprep.X_validate, ere_dataprep.Y_validate))
+        best_block_output = [bs, l, current_e_values[0], current_block]
+        with open("results/output_info/output_info.csv", "a", newline="") as output_info:
+            output_writer = csv.writer(output_info)
+            output_writer.writerow(best_block_output)
+        sys.stdout = console_output
+        file_output.close()
